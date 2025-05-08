aa62439a33901c4c7e8671a6b9fee6346e5f89a7
jasmine graciano <jasminegraciano@MacBook-Pro-2.local>
2025-05-08 16:13:03 -0400

    portfolio project

--- components/SubmitAlertForm.jsx
+++ components/SubmitAlertForm.jsx
@@ -107,7 +107,7 @@ const SubmitAlertForm = () => {
   const onPlaceSelect = (place) => {
     console.log('> New Place: ', place);
     const newFormData = {
-      address: place.name,
+      address: place.formatted_address,
       latitude: place.geometry.location.lat(),
       longitude: place.geometry.location.lng(),
       category: formData.category,
@@ -200,21 +200,23 @@ const SubmitAlertForm = () => {
         />
       </div>
 
-      {/* <div>
+      <div>
         <label className="block text-sm font-medium text-gray-700">Address</label>
-        <input
-          type="text"
-          name="address"
-          value={formData.address}
-          onChange={handleChange}
-          required
-          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-        />
-      </div> */}
-      <div className='autocomplete-container'>
-        <input ref={inputRef} onSubmit={(e) => {
-          e.preventDefault();
-        }} />
+        <div className="autocomplete-container">
+          <input
+            ref={inputRef}
+            type="text"
+            name="address"
+            value={formData.address}
+            onChange={handleChange}
+            required
+            placeholder="Search for an address..."
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          />
+        </div>
       </div>
 
       <button
